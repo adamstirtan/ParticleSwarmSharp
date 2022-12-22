4cd241f540a7cb15cff47eebf64cd65def1572b5
Adam Stirtan <adamstirtan@gmail.com>
2022-12-21 19:28:54 -0500

    Added unit test project

---
+++ src/ParticleSwarm.Tests/OptimizationResultTest.cs
@@ -0,0 +1,23 @@
+namespace ParticleSwarm.Tests
+{
+    [TestClass]
+    public class OptimizationResultTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ParticleSwarmOptions options = new()
+            {
+                PopulationSize = 100,
+                Iterations = 25,
+                Topology = Topologies.Star
+            };
+
+            ParticleSwarm pso = new(options);
+
+            OptimizationResult result = pso.Optimize();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
